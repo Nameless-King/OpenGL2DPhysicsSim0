4be6e43929db1ff8528958b5c428e8b6e2a268ab
Tim D <timothy.deutsch@yahoo.com>
2019-10-02 12:16:50 -0600

    Starting code for object class to hold info on an object rendered on the screen

---
+++ Object.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#include "VertexArrayObj.h"
+#include "./Dependencies/glm/glm.hpp"
+#include "./Dependencies/glm/gtc/matrix_transform.hpp"
+#include "./Dependencies/glm/gtc/type_ptr.hpp"
+
+class Object{
+private:
+	VertexArrayObj m_vao;
+	glm::vec3 m_position;
+	glm::vec3 m_rotation;
+	glm::vec3 m_scale;
+public:
+	Object(VertexArrayObj vao,glm::vec3 position, glm::vec3 rotation, glm::vec3 scale);
+	~Object();
+	
+	glm::mat4 getModelMatrix();
+	
+	
+	
+};
