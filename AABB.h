d265527c29a624bb2c448ae441c742d4b07b183b
Tim D <timothy.deutsch@yahoo.com>
2019-10-02 21:15:08 -0600

    Header files to control collision detection have been created but are not fully functional

---
+++ AABB.h
@@ -0,0 +1,11 @@
+#pragma once
+
+//struct or whatever to hold intersection data
+
+class AABB{//or alt class for circle stuff
+	//- float:halfExtent_x
+	//- float:halfExtent_y
+	//- vec2:maxCoord
+	//- vec2:minCoord
+};
+	
---
+++ Rectangle.h
@@ -0,0 +1,9 @@
+class Rectangle{//extends obj (transformations/VAO
+private:
+	//AABB or Cirlce stuffs
+public:
+	Rectangle(const void* vertices);
+	~Rectangle();
+	
+	
+};
