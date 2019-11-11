d05b660178ceaa529eb789bc6bc9420f3244a31a
Tim D <timothy.deutsch@yahoo.com>
2019-11-10 23:58:44 -0600

    Force to simulate a spring attached to an anchored point

---
+++ ForceAnchoredSpring.cpp
@@ -0,0 +1,38 @@
+#include "./ForceAnchoredSpring.h"
+
+ForceAnchoredSpring::ForceAnchoredSpring(float px, float py,float springConstant, float equilibrium):
+    m_springConstant(springConstant),
+    m_equilibrium(equilibrium){
+    m_anchor = new glm::vec2(px,py);
+    }
+
+ForceAnchoredSpring::~ForceAnchoredSpring(){
+    delete m_anchor;
+}
+
+void ForceAnchoredSpring::updateForce(Object* object, float dt){
+    glm::vec2 force;
+    object->getPos(force);
+    force -= * m_anchor;
+
+    float magnitude = glm::length(force);
+    magnitude = fabs(magnitude - m_equilibrium);
+    magnitude *= m_springConstant;
+
+    glm::normalize(force);
+    force *= -magnitude;
+    object->getRigidBody2D()->addForce(force);
+}
+
+void ForceAnchoredSpring::setAnchor(float px, float py){
+    m_anchor->x = px;
+    m_anchor->y = py;
+}
+
+void ForceAnchoredSpring::setSpringConstant(float k){
+    m_springConstant = k;
+}
+
+void ForceAnchoredSpring::setEquilibrium(float equilibrium){
+    m_equilibrium = equilibrium;
+}
